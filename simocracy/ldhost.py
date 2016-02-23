8890f7bb37a513286c1788c82b3f3ded391313ae
Fluggs <christian@emailgsm.de>
2016-02-23 18:14:29 +0100

    ldhost-Skript: erste Fassung

--- simocracy/ldhost.py
+++ simocracy/ldhost.py
@@ -2,8 +2,80 @@
 # -*- coding: UTF-8 -*-
 
 import simocracy.wiki as wiki
+import re
 
-opener = wiki.login(wiki.username, wiki.password)
+## config ##
+simulation = True
+#Loglevel: schreibe nur geänderte Zeilen ("line") oder
+#          ganze geänderte Artikel ("article") auf stdin
+loglevel = "line"
+############
 
-for p in wiki.allPages(opener):
-    print(p)
+
+def main():
+    opener = wiki.login(wiki.username, wiki.password)
+
+    for p in wiki.allPages(opener):
+        doIt(p, opener)
+
+#Ersetzt alle Vorkommnisse von sub in s durch repl.
+def replaceAll(sub, repl, s):
+    while True:
+        testagainst = s
+        s = re.sub(sub, repl, s)
+        if s == testagainst:
+            return s
+
+def doIt(article, opener):
+    replacement = re.escape("hier war ein LD-Host-Link")
+    notif = re.escape("{{LD-Host|--~~~~}}")
+    ldhost = re.compile(r'((http://)?(www.)?ld-host.de/[/\w]*?\.[a-z][a-z][a-z])')
+    found = False
+    text = ""
+    logs = ""
+
+    for line in wiki.openArticle(article, opener):
+        newLine = line.decode('utf-8')
+        m = ldhost.findall(newLine)
+        foundList = []
+        for el in m:
+            foundList.append(el[0])
+
+        #nichts gefunden
+        if foundList == []:
+            text = text + newLine + "\n"
+            continue
+        else:
+            found = True
+
+        #ersetzen
+        for el in foundList:
+            newLine = replaceAll(el, replacement, newLine)
+
+        text = text + newLine + "\n"
+
+        #logging
+        if simulation and loglevel == "line":
+            logs = logs + "\n- " + line.decode('utf-8') + "+ " + newLine
+
+    if found:
+        text = notif + "\n" + text
+
+        if not simulation:
+            wiki.editArticle(article, text, opener)
+        #Simulation
+        else:
+            print("[[" + article + "]]")
+            if loglevel == "line":
+                print(logs)
+            elif loglevel == "article":
+                print(text)
+            else:
+                raise Exception("config kaputt")
+
+            print("========================================================\n\n")
+
+        
+
+if __name__ == "__main__":
+    main()
